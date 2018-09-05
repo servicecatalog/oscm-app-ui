989b7ff2f10bdd4e9c23d2a7404092d5652a662a
Sebastian Florek <Sebastian.Florek@ts.fujitsu.com>
2018-09-05 14:08:15 +0200

    Add typings and rename sass to styles

---
+++ src/typings/api.d.ts
@@ -0,0 +1,40 @@
+export enum ProvisioningStatus {
+  WAITING_FOR_SYSTEM_CREATION = 1,
+  COMPLETED = 2,
+}
+
+export interface InstanceAttribute {
+  id: number;
+  key: string;
+  value: string;
+}
+
+export interface InstanceParameter {
+  id: number;
+}
+
+export interface Instance {
+  id: number;
+  controllerId: string;
+  instanceId: string;
+  attributes: Array<InstanceAttribute>;
+  organizationName: string;
+  parameters: Array<InstanceParameter>;
+  provisioningMsg: string;
+  provisioningStatus: string | ProvisioningStatus;
+  referenceId: string;
+  requestTime: string | Date;
+  subscriptionId: string;
+}
+
+export interface Controller {
+  controllerId: string;
+  description: string;
+}
+
+export interface Configuration {
+  id: number;
+  description: string;
+  controllerId: string;
+  organizationId: string;
+}
