4c8e0c0ded92e87fc37e50bcf3a780d4253c08fa
Marcin Maciaszczyk <marcin.maciaszczyk@ts.fujitsu.com>
2018-09-05 14:06:58 +0200

    Add shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SharedModule} from '../shared.module';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/shared.module.ts
@@ -0,0 +1,40 @@
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {MAT_TOOLTIP_DEFAULT_OPTIONS, MatButtonModule, MatButtonToggleModule, MatCardModule, MatChipsModule, MatDialogModule, MatDividerModule, MatFormFieldModule, MatGridListModule, MatIconModule, MatInputModule, MatMenuModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, MatRadioModule, MatSelectModule, MatSidenavModule, MatSliderModule, MatSlideToggleModule, MatSnackBarModule, MatSortModule, MatTableModule, MatTabsModule, MatToolbarModule, MatTooltipModule} from '@angular/material';
+
+const SHARED_DEPENDENCIES = [
+  CommonModule,
+  FormsModule,
+  MatButtonModule,
+  MatCardModule,
+  MatDividerModule,
+  MatGridListModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatProgressSpinnerModule,
+  MatProgressBarModule,
+  MatRadioModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatSliderModule,
+  MatDialogModule,
+  MatSlideToggleModule,
+  MatChipsModule,
+  MatTableModule,
+  MatSortModule,
+  MatPaginatorModule,
+  MatTabsModule,
+  MatMenuModule,
+  MatSelectModule,
+  MatButtonToggleModule,
+  MatSnackBarModule,
+];
+
+@NgModule({
+  imports: SHARED_DEPENDENCIES,
+  exports: SHARED_DEPENDENCIES,
+})
+export class SharedModule {}
